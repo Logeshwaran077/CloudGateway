2fac3421015b150920a19e1bd5b3ec82418201d9
Logeshwaran07 <128709320+Logeshwaran07@users.noreply.github.com>
2026-03-08 17:55:00 +0530

    initial commit

---
+++ src/main/java/com/learnmicroservices/CloudGateway_lrmicro/controller/FallBackController.java
@@ -0,0 +1,25 @@
+package com.learnmicroservices.CloudGateway_lrmicro.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+
+
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallback(){
+        return "Order service is down";
+    }
+
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallback(){
+        return "Payment service is down";
+    }
+
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallback(){
+        return "Product service is down";
+    }
+}
---
+++ src/test/java/com/learnmicroservices/CloudGateway_lrmicro/CloudGatewayLrmicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnmicroservices.CloudGateway_lrmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayLrmicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
